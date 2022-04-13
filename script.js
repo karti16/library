a4ea61ca55629037caad2161f758d4ccec444a35
karti16 <kartim316@gmail.com>
2022-04-13 12:35:02 +0530

    support for local storage

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let my;
+if (localStorage.key("book") == null) {
+  localStorage.setItem("book", JSON.stringify([]));
+}
+
+if (localStorage.key("book") == "book") {
+  myLibrary = JSON.parse(localStorage.getItem("book"));
+}
 
 const libContainer = document.querySelector(".lib-container");
 const bookForms = document.getElementById("bookForms");
@@ -24,14 +30,10 @@ function addBookToLibrary(e) {
       document.getElementById("isRead").checked ? "yes" : "no"
     )
   );
-
-  updateBookBtn.style.display = "none";
-  addBookBtn.style.display = "block";
-
+  updateLocalStorage();
   displayBook();
-  bookForms.reset();
   overlayOff();
-  localStorage.setItem("book", JSON.stringify(myLibrary));
+  bookForms.reset();
 }
 
 function dynamicButton(e) {
@@ -47,41 +49,45 @@ function dynamicButton(e) {
     editBook(e);
   }
 }
-
+function updateLocalStorage() {
+  localStorage.setItem("book", JSON.stringify(myLibrary));
+}
 function delBookFromLibrary(e) {
   let bookDel = document.getElementById(getId(e));
   bookDel.remove();
   myLibrary.splice(getId(e), 1);
+  updateLocalStorage();
+  updateBookGrid();
 }
 
 function toggleRead(e) {
   let book = document.getElementById(getId(e));
   let status = book.querySelector("#toggleRead");
-  let readStatus = book.querySelector("#readStatus");
-
   if (myLibrary[getId(e)].isRead == "yes") {
-    status.innerHTML = '<span class="mdi mdi-book-remove"></span>';
-    readStatus.innerHTML = "Read ? : no";
+    status.innerHTML = "Mark as Read";
     myLibrary[getId(e)].isRead = "no";
   } else {
-    status.innerHTML = '<span class="mdi mdi-book-check"></span>';
-    readStatus.innerHTML = "Read ? : yes";
+    status.innerHTML = "Mark as Unread";
     myLibrary[getId(e)].isRead = "yes";
   }
+  updateLocalStorage();
 }
 
 function editBook(e) {
+  console.log(getId(e));
   document.querySelector("#title").value = myLibrary[getId(e)].title;
   document.querySelector("#author").value = myLibrary[getId(e)].author;
   document.querySelector("#pages").value = myLibrary[getId(e)].pages;
   document.querySelector("#isRead").checked =
     myLibrary[getId(e)].isRead == "yes" ? true : false;
 
+  overlayOn();
+
   updateBookBtn.style.display = "block";
   addBookBtn.style.display = "none";
 
-  overlayOn();
   currentBookEdit = getId(e);
+  updateLocalStorage();
 }
 
 function updateBook(e) {
@@ -94,28 +100,34 @@ function updateBook(e) {
 
   updateBookGrid();
   overlayOff();
+  updateLocalStorage();
 }
 
 function displayBook() {
+  myLibrary = JSON.parse(localStorage.getItem("book"));
   let libContainer = document.querySelector(".lib-container");
-  libContainer.innerHTML += `<div class="book" id="${bookId}">
-          <ul>
-            <li>Title : ${myLibrary[bookId].title}</li>
-            <li>Author : ${myLibrary[bookId].author}</li>
-            <li>Pages : ${myLibrary[bookId].pages}</li>
-            <li id="readStatus">Read ? : ${myLibrary[bookId].isRead}</li>
-            <li><button id="delBook" class="delBook"><span class="mdi mdi-delete"></span></button></li>
-            <li><button id="toggleRead">${
-              myLibrary[bookId].isRead == "yes"
-                ? '<span class="mdi mdi-book-check"></span>'
-                : '<span class="mdi mdi-book-remove"></span>'
-            }</button></li>
-           <li> <button id="editBook"><span class="mdi mdi-pencil"></span></button></li>
-          </ul>
-        </div>`;
+  libContainer.innerHTML += `
+    <div class="book" id="${bookId}">
+        <div>Title : ${myLibrary[bookId].title}</div>
+        <div>Author : ${myLibrary[bookId].author}</div>
+        <div>Pages : ${myLibrary[bookId].pages}</div>
+        <p id="toggleRead">
+          ${
+            myLibrary[bookId].isRead == "yes"
+              ? "Mark as Unread"
+              : "Mark as Read"
+          }
+        </p>
+        <div id="delEditBtn">
+          <i id="delBook" class="material-icons">delete</i>
+          <i id="editBook" class="material-icons">edit</i>
+        </div>
+    </div>`;
 }
 
 function overlayOn() {
+  updateBookBtn.style.display = "none";
+  addBookBtn.style.display = "block";
   document.getElementById("overlay").style.display = "block";
 }
 
@@ -125,27 +137,25 @@ function overlayOff() {
 }
 
 function getId(e) {
-  return e.target.parentNode.parentNode.parentElement.id;
+  return e.target.parentElement.id;
 }
 
 function updateBookGrid() {
   libContainer.innerHTML = "";
   for (let i = 0; i < myLibrary.length; i++) {
-    libContainer.innerHTML += `<div class="book" id="${i}">
-          <ul>
-            <li>Title : ${myLibrary[i].title}</li>
-            <li>Author : ${myLibrary[i].author}</li>
-            <li>Pages : ${myLibrary[i].pages}</li>
-            <li id="readStatus">Read ? : ${myLibrary[i].isRead}</li>
-            <li><button id="delBook" class="delBook"><span class="mdi mdi-delete"></span></button></li>
-            <li><button id="toggleRead">${
-              myLibrary[i].isRead == "yes"
-                ? '<span class="mdi mdi-book-check"></span>'
-                : '<span class="mdi mdi-book-remove"></span>'
-            }</button></li>
-           <li> <button id="editBook"><span class="mdi mdi-pencil"></span></button></li>
-          </ul>
-        </div>`;
+    libContainer.innerHTML += `
+    <div class="book" id="${i}">
+        <div>Title : ${myLibrary[i].title}</div>
+        <div>Author : ${myLibrary[i].author}</div>
+        <div>Pages : ${myLibrary[i].pages}</div>
+        <p id="toggleRead">
+          ${myLibrary[i].isRead == "yes" ? "Mark as Unread" : "Mark as Read"}
+        </p>
+        <div id="delEditBtn">
+          <i id="delBook" class="material-icons">delete</i>
+          <i id="editBook" class="material-icons">edit</i>
+        </div>
+    </div>`;
   }
 }
 
@@ -154,6 +164,3 @@ updateBookGrid();
 document.onclick = dynamicButton;
 bookForms.onsubmit = addBookToLibrary;
 updateBookBtn.onclick = updateBook;
-
-localStorage.setItem("book", JSON.stringify(myLibrary));
-let storedNames = JSON.parse(localStorage.getItem("names"));
